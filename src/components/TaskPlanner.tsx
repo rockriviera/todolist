f177908da2d243ce81feb49dcfa6b4446000924b
Isak s <isak.shamun@gmail.com>
2024-04-13 21:59:11 +0200

    add search functionality and reorganize TaskPlanner.tsx

--- src/components/TaskPlanner.tsx
+++ src/components/TaskPlanner.tsx
@@ -1,27 +1,54 @@
-import {Task} from "../interfaces/Task.ts";
+import { Task } from "../interfaces/Task.ts";
+import { useState } from "react";
 
 export default function TaskPlanner() {
-    const TASKS = [
-        {description: "Brush teeth"},
-        {description: "Polish mirror"},
-        {description: "Rescue Cat"},
-        {description: "Save the citizens of Metropolis"},
-    ];
-
-    function TaskRow({task}: { task: Task }) {
-        return (
-            <li>{task.description}</li>
-        )
-    }
-
-    function TaskList({tasks}: { tasks: Task[] }) {
-        const taskList = tasks.map((task => <TaskRow task={task} key={task.description}/>))
-        return (<ul className='list-style: none;'>{taskList}</ul>)
-    }
-
-    return (
-        <>
-            <TaskList tasks={TASKS}/>
-        </>
-    )
+  const [taskFilter, setTaskFilter] = useState("");
+  const TASKS = [
+    { description: "Brush teeth" },
+    { description: "Polish mirror" },
+    { description: "Rescue Cat" },
+    { description: "Save the citizens of Metropolis" },
+  ];
+  return (
+    <>
+      <SearchBar searchQuery={taskFilter} onSearchQueryChange={setTaskFilter} />
+      <TaskList tasks={TASKS} taskFilter={taskFilter} />
+    </>
+  );
+}
+function SearchBar({
+  searchQuery,
+  onSearchQueryChange,
+}: {
+  searchQuery: string;
+  onSearchQueryChange: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  return (
+    <input
+      type="text"
+      value={searchQuery}
+      placeholder="Search..."
+      onChange={(e) => {
+        onSearchQueryChange(e.target.value);
+      }}
+    />
+  );
+}
+function TaskList({
+  tasks,
+  taskFilter,
+}: {
+  tasks: Task[];
+  taskFilter: string;
+}) {
+  const filteredTasks = tasks.filter(
+    (task) => task.description.indexOf(taskFilter) !== -1
+  );
+  const filteredTaskList = filteredTasks.map((task) => (
+    <TaskRow task={task} key={task.description} />
+  ));
+  return <ul>{filteredTaskList}</ul>;
+}
+function TaskRow({ task }: { task: Task }) {
+  return <li>{task.description}</li>;
 }
